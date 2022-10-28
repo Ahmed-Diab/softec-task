abd54c6bc15b0546572c80e1cfdb6d0da482c091
ahmed-diab <ahmedeiddiab@gmail.com>
2022-10-28 22:51:00 +0200

    add product card and table codes componet html and css and fix product table issus

--- src/app/products/product-card/product-card.component.ts
+++ src/app/products/product-card/product-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IProduct } from '../iproduct';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
+  @Input() products: IProduct[];
+  @Output() UpdateQuntityEvent = new EventEmitter<IProduct>();
+  @Output() AddToOrderEvent = new EventEmitter<IProduct>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  openAvailablePiecesModal(product: IProduct) {
+    this.UpdateQuntityEvent.emit(product);
   }
-
+  
+  addProductToOrder(product: IProduct) {
+    this.AddToOrderEvent.emit(product);
+  }
 }
--- src/app/products/product-table/product-table.component.ts
+++ src/app/products/product-table/product-table.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IProduct } from '../iproduct';
+ import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-product-table',
   templateUrl: './product-table.component.html',
   styleUrls: ['./product-table.component.scss']
 })
-export class ProductTableComponent implements OnInit {
+export class ProductTableComponent  implements OnInit {
+  @Input() products: IProduct[];
+  @Output() UpdateQuntityEvent = new EventEmitter<IProduct>();
+  @Output() AddToOrderEvent = new EventEmitter<IProduct>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  openAvailablePiecesModal(product: IProduct) {
+    this.UpdateQuntityEvent.emit(product);
+  }
+  addProductToOrder(product: IProduct) {
+    this.AddToOrderEvent.emit(product);
   }
 
 }

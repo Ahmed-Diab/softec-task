37269512cc3268062c5eea771145514995244509
ahmed-diab <ahmedeiddiab@gmail.com>
2022-10-28 20:59:00 +0200

    create product table and product card component

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -5,9 +5,11 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { FormsModule } from '@angular/forms';
 import { ProductService } from './product.service';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductTableComponent } from './product-table/product-table.component';
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, ProductCardComponent, ProductTableComponent],
   imports: [CommonModule, FormsModule, ProductsRoutingModule],
   providers: [ProductService],
 })

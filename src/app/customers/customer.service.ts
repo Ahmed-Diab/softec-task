840e54ed056c14a79f8479d8112ee20ba1f3a1fb
ahmed-diab <ahmedeiddiab@gmail.com>
2022-10-29 06:26:04 +0200

    add getCustomers method to customer service

--- src/app/customers/customer.service.ts
+++ src/app/customers/customer.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICustomer } from './icustomer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(
+    private httpClient:HttpClient
+  ) { }
+
+  getCustomers():Observable<ICustomer[]>{
+    return this.httpClient.get<ICustomer[]>('users.json')
+  }
+
 }

d2b0e9d8a23aa2cc24ff83c7f492f516927f2e18
ahmed-diab <ahmedeiddiab@gmail.com>
2022-10-29 06:19:47 +0200

    add customers module to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomersComponent } from './customers/customers/customers.component';
 
 const routes: Routes = [
-  {path:'', redirectTo:'products', pathMatch:'full'},
   {
     path: 'products',
     loadChildren: () =>
-      import('./products/products.module').then(
-        (m) => m.ProductsModule
-      ),
+      import('./products/products.module').then((m) => m.ProductsModule),
   },
   {
     path: 'orders',
     loadChildren: () =>
-      import('./orders/orders.module').then(
-        (m) => m.OrdersModule
-      ),
+      import('./orders/orders.module').then((m) => m.OrdersModule),
   },
+  {
+    path:'customers', 
+    loadChildren:()=> 
+       import('./customers/customers.module').then((m)=>m.CustomersModule)
+  },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CustomersComponent } from './customers/customers.component';
+ 
+const routes: Routes = [
+  { path: '', component: CustomersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CustomersRoutingModule { }
+export class CustomersRoutingModule {}
